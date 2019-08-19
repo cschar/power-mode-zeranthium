47aff0c5fbda534a11fdffd8bcd2057de8074775
cschar <codyscharfe@gmail.com>
2019-08-19 19:30:17 -0400

    clean up vine sin/cos path

--- src/main/java/com/cschar/pmode3/ParticleSpriteVineAnchor.java
+++ src/main/java/com/cschar/pmode3/ParticleSpriteVineAnchor.java
@@ -17,16 +17,10 @@
 
 package com.cschar.pmode3;
 
-import org.imgscalr.Scalr;
-
-import javax.imageio.ImageIO;
 import java.awt.*;
-import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.logging.Logger;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class ParticleSpriteVineAnchor extends Particle{
 
@@ -48,12 +42,15 @@ public class ParticleSpriteVineAnchor extends Particle{
 
     double radius;
 
-    float initAngle;
-    float initAnchorAngle;
-    float curAngle;
+    double initAngle;
+    double initAnchorAngle;
+    double curAngle;
+    double adjacent;
 
     double K_initAnchorAngle;
 
+    boolean spawnsFromBelow = false;
+    boolean spawnsFromRight = false;
     public ParticleSpriteVineAnchor(int x, int y, int dx, int dy,
                                     int anchorX, int anchorY, int size, int life, Color c) {
         super(x,y,dx,dy,size,life,c);
@@ -66,99 +63,103 @@ public class ParticleSpriteVineAnchor extends Particle{
         this.initialX = x;
         this.initialY = y;
 
+        if(anchorY > initialY) {
+            spawnsFromBelow = true;
+        }
+        if(anchorX > initialX) {
+            spawnsFromRight = true;
+        }
+
         this.dir2anchorX = ((anchorX - initialX)/70);
         this.dir2anchorY = (anchorY - initialY)/70;
+
+
 
         midPoint = new Point((anchorX + initialX)/2,
                              (anchorY + initialY)/2);
 
+
+
         radius =  Math.sqrt(Math.pow((double) (midPoint.x - anchorX), 2.0f) +
                            Math.pow((double)  (midPoint.y - anchorY),2.0f));
 
-        float adjacent = (x - midPoint.x);
-
-        initAngle = (float) Math.cosh(adjacent/radius);
-        curAngle = initAngle;
+         adjacent = (anchorX - midPoint.x);
+        initAnchorAngle = Math.acos(adjacent/radius);
 
 
-        float anchorAdjacent = (anchorX - midPoint.x);
-        initAnchorAngle = (float) Math.cosh(anchorAdjacent/radius);
-//        if(anchorAdjacent > 0){  // anchor is on Right side of caret
-            initAnchorAngle -= Math.PI;
-//        }
-
-//        if(anchorX > initialX) {
-//            K_initAnchorAngle = Math.cosh((anchorX - midPoint.x) / radius);
-//        }else if(anchorX < initialX){
-//            K_initAnchorAngle = Math.cosh(())
-//        }
+        curAngle = initAnchorAngle;
 
         prevPoints = new ConcurrentLinkedQueue<Point>();
+
+
+        randYSquiggleOffset = ThreadLocalRandom.current().nextDouble(0.0f, 1.0f);
+        randXSquiggleOffset = ThreadLocalRandom.current().nextDouble(0.0f, 1.0f);
     }
 
 
+    double randYSquiggleOffset;
+    double randXSquiggleOffset;
     ConcurrentLinkedQueue<Point> prevPoints;
 
     double angleDiff = 0.0f;
+    boolean hasFoundEnd = false;
     public boolean update() {
 
 
-        //wtf? why does sin work for "x"
-        float circleX = radius * (float) Math.sin(curAngle);
-        float circleY = radius * (float) Math.cos(curAngle);
-
-
-
-//        angleDiff = (float) Math.abs(Math.abs(curAngle % Math.PI) - Math.abs(initAnchorAngle));
-//        double degDiff = Math.abs(Math.toDegrees(curAngle) - Math.toDegrees(initAnchorAngle));
-        double tmpAnchorAngle = (initAnchorAngle + 2*Math.PI) % (2*Math.PI);
-        double tmpcurAngle = curAngle % (2*Math.PI);
-
-
-            angleDiff = (tmpAnchorAngle - tmpcurAngle);
-//        double radDiff = Math.abs((curAngle % (Math.PI)) - (initAnchorAngle % (Math.PI)));
-//
-//        if(life % 2 == 0 && radDiff > 0.1f) {
-//            curAngle += 0.05;
-//        }
-
-//        if(life % 2 == 0 && degDiff > 1) {
-//            curAngle += 0.05;
-//        }
-
-        // -'ve radian --> clockwise
-
-        if(anchorY > initialY) {
-            if(life % 2 == 0 && (angleDiff % (Math.PI*2)) > 0.06f) {
-                curAngle -= 0.05;  // clockwise  --  on bottom
+        if(life % 2 == 0 && !hasFoundEnd    ) {
+            double circleX = radius * (float) Math.cos(curAngle);
+            double circleY = radius * (float) Math.sin(curAngle);
+            if(!spawnsFromBelow){
+                circleY = radius * -1 * (float) Math.sin(curAngle); //Why?
             }
-        }else{
-            if(life % 2 == 0 && (angleDiff) > 0.06f) {
-                curAngle += 0.05;  //counterclockwise -- on top
+
+//            circleY *= 0.5f; //make general path more oval
+
+            int tmpx = (int) circleX + midPoint.x;
+            int tmpy = (int) circleY + midPoint.y;
+            if(isNearEnd(tmpx,tmpy)) {
+                hasFoundEnd = true;
+            }else{
+                x = tmpx;
+                y = tmpy;
             }
-        }
-
-        if(!(anchorX < initialX)) {// only kill them if they are on left side
-            if (Math.abs(x - anchorX) < 7 && Math.abs(y - anchorY) < 7) {
-                life = 0;
+            float angleIncr = 0;
+            if(life < maxLife - 10) {
+                //add squiggle movement
+                x += ((radius / 10) + 5 * randXSquiggleOffset) * Math.sin((5 * randXSquiggleOffset) * curAngle);
+                y += (radius / 10) * Math.sin((5 * randYSquiggleOffset) * curAngle);
             }
-        }
+            if(spawnsFromBelow) {
+                angleIncr = -0.05f;
+            }else {
+                angleIncr = -0.05f;
+            }
+
+            //so particles dont go infront of text, less Looping aesthetic though
+//                if(spawnsFromRight && !spawnsFromBelow){
+//                    angleIncr = 0.05f;
+//                }
+//                if(spawnsFromRight && spawnsFromBelow){
+//                    angleIncr = 0.05f;
+//                }
 
 
-//        if( angleDiff < 0.3f){
-//            life = 0;
-//        }
-
-        x = (int) circleX + midPoint.x;
-        y = (int) circleY + midPoint.y;
-
-        prevPoints.add(new Point(x,y));
+            curAngle += angleIncr;  // clockwise  --  on bottom
+            prevPoints.add(new Point(x,y));
+        } //if life % 2 == 0
 
 
         life--;
         return life <= 0;
     }
 
+    boolean isNearEnd(int x, int y){
+        if(spawnsFromBelow){ //add height for caretline
+            return (Math.abs(x - initialX) < 15 && Math.abs(y - (initialY + 20)) < 15);
+        }else{
+            return (Math.abs(x - initialX) < 15 && Math.abs(y - initialY) < 15);
+        }
+    }
 
 
     public void render(Graphics g) {
@@ -168,17 +169,14 @@ public class ParticleSpriteVineAnchor extends Particle{
 //            final AffineTransform identity = new AffineTransform();
 //            g2d.setTransform(identity);
 
+
             g2d.setColor(c);
             if(anchorY > initialY) {
                 g2d.setColor(Color.CYAN);
             }
-//            g2d.drawString(String.format("cur: %.3f",curAngle), midPoint.x - 10, midPoint.y);
-            g2d.drawString(String.format("anglediff %f ", angleDiff), midPoint.x - 20, midPoint.y-30);
-            g2d.drawString(String.format("initAnchorAngle %.3f ", (initAnchorAngle + 2*Math.PI) % (2*Math.PI)), midPoint.x - 20, midPoint.y-50);
 
-//
-//            g2d.drawString(String.format("initAnchor: %.3f", initAnchorAngle), anchorX, anchorY);
-//            g2d.fillRect(x - (size / 2), y - (size / 2), size, size);
+            g2d.drawString(String.format("anchorAngle %.3f (cur: %.3f) ", initAnchorAngle, curAngle), anchorX - 20, anchorY-30);
+
             g2d.fillRect(x - (8 / 2), y - (8 / 2), 8, 8);
 
             for( Point p: prevPoints){
@@ -187,6 +185,10 @@ public class ParticleSpriteVineAnchor extends Particle{
 
             g2d.setColor(Color.RED);
             g2d.fillRect(midPoint.x - (8 / 2), midPoint.y - (8 / 2), 8, 8);
+
+            g2d.setColor(Color.ORANGE);
+            g2d.fillRect(anchorX - (8 / 2), anchorY - (8 / 2), 8, 8);
+
 
             g2d.dispose();
         }
